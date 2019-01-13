e93afda6e61c56f818fb521bf5ce19d2d7e54d6c
Athos Bacchiocchi <athos.bacchiocchi@gmail.com>
2019-01-13 19:14:27 +0800

    window resize

--- main.cpp
+++ main.cpp
@@ -10,10 +10,17 @@
 static sk_sp<GrContext> sContext;
 static sk_sp<SkSurface> sSurface;
 
-void init_skia(int w, int h) {
+void init_gr_context() {
     GrContextOptions options;
     //options.fRequireDecodeDisableForSRGB = false;
     sContext = GrContext::MakeGL(nullptr);//, options);
+}
+
+void init_surface(int w, int h) {
+
+    if (sSurface != nullptr) {
+        sSurface.reset();
+    }
 
     GrGLFramebufferInfo framebufferInfo;
     framebufferInfo.fFBOID = 0; // assume default framebuffer
@@ -21,99 +28,86 @@ void init_skia(int w, int h) {
     //(replace line below with this one to enable correct color spaces) framebufferInfo.fFormat = GL_SRGB8_ALPHA8;
     framebufferInfo.fFormat = GL_RGBA8;
 
-    SkColorType colorType;
-    if (kRGBA_8888_GrPixelConfig == kSkia8888_GrPixelConfig) {
-        colorType = kRGBA_8888_SkColorType;
-    }
-    else {
-        colorType = kBGRA_8888_SkColorType;
-    }
-    GrBackendRenderTarget backendRenderTarget(w, h,
-        0, // sample count
-        0, // stencil bits
-        framebufferInfo);
+    SkColorType colorType = kRGBA_8888_GrPixelConfig == kSkia8888_GrPixelConfig ? kRGBA_8888_SkColorType : kBGRA_8888_SkColorType;
+    GrBackendRenderTarget backendRenderTarget(w, h, 0 /* sample count */, 0 /* stencil bits */, framebufferInfo);
 
-    //(replace line below with this one to enable correct color spaces) sSurface = SkSurface::MakeFromBackendRenderTarget(sContext, backendRenderTarget, kBottomLeft_GrSurfaceOrigin, colorType, SkColorSpace::MakeSRGB(), nullptr).release();
+    // (replace sSurface creation with this one to enable correct color spaces):
+    // sSurface = SkSurface::MakeFromBackendRenderTarget(sContext, backendRenderTarget, kBottomLeft_GrSurfaceOrigin, colorType, SkColorSpace::MakeSRGB(), nullptr).release();
 
     sSurface = SkSurface::MakeFromBackendRenderTarget(sContext.get(), backendRenderTarget, kBottomLeft_GrSurfaceOrigin, colorType, nullptr, nullptr);
     if (sSurface == nullptr)
         abort();
 }
 
-void gl_example(int width, int height) {
-    // You've already created your OpenGL context and bound it.
-    const GrGLInterface* interface = nullptr;
-    // Leaving interface as null makes Skia extract pointers to OpenGL functions for the current
-    // context in a platform-specific way. Alternatively, you may create your own GrGLInterface and
-    // initialize it however you like to attach to an alternate OpenGL implementation or intercept
-    // Skia's OpenGL calls.
-
-    sContext = GrContext::MakeGL(nullptr);//GrContext::MakeGL(interface);
-    SkImageInfo info = SkImageInfo:: MakeN32Premul(width, height);
-    sSurface = SkSurface::MakeRenderTarget(sContext.get(), SkBudgeted::kNo, info);
-    if (!sSurface) {
-        SkDebugf("SkSurface::MakeRenderTarget returned null\n");
-        return;
-    }
+void draw_content() {
+    SkCanvas* canvas = sSurface->getCanvas();
+    canvas->clear(SK_ColorRED);
+    SkRect r = SkRect::MakeXYWH(1,1,598,398);
+    SkPaint p;
+    p.setColor(SK_ColorGREEN);
+    canvas->drawRect(r,p);
+    r.setXYWH(1,1,298,198);
+    p.setColor(SK_ColorBLUE);
+    canvas->drawRect(r,p);
+    sContext->flush();
 }
 
-void another_skia_init() {
-//    // You've already created your OpenGL context and bound it.
-//    sk_sp<const GrGLInterface> interface = nullptr;
-//    // Leaving interface as null makes Skia extract pointers to OpenGL functions for the current
-//    // context in a platform-specific way. Alternatively, you may create your own GrGLInterface and
-//    // initialize it however you like to attach to an alternate OpenGL implementation or intercept
-//    // Skia's OpenGL calls.
-
-//    sk_sp<GrContext> context = GrContext::MakeGL(interface);
-
-
-//    SkImageInfo info = SkImageInfo:: MakeN32Premul(300, 300);
-//    sk_sp<SkSurface> gpuSurface(
-//            SkSurface::MakeRenderTarget(context, SkBudgeted::kNo, info));
-//    if (!gpuSurface) {
-//        SkDebugf("SkSurface::MakeRenderTarget returned null\n");
-//        return;
-//    }
-//    SkCanvas* gpuCanvas = gpuSurface->getCanvas();
-//    draw(gpuCanvas);
-//    sk_sp<SkImage> img(gpuSurface->makeImageSnapshot());
-//    if (!img) { return; }
-//    sk_sp<SkData> png(img->encode());
-//    if (!png) { return; }
-//    SkFILEWStream out(path);
-//    (void)out.write(png->data(), png->size());
-
+void fbCallback(GLFWwindow* window,int w,int h) {
+    printf("FB w %d, h %d\n",w,h);
+    init_surface(w,h);
+    draw_content();
+    glfwSwapBuffers(window);
 }
+
+//void sizeCallback(GLFWwindow* window,int w,int h) {
+//    printf("WIN w %d, h %d\n",w,h);
+//    init_skia(w,h);
+//    draw_content();
+//    glfwSwapBuffers(window);
+//}
 
 int main(void)
 {
+    printf("hello\n");
     GLFWwindow* window;
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    window = glfwCreateWindow(640, 480, "Skia on GLFW", nullptr, nullptr);
+    window = glfwCreateWindow(600, 400, "Skia on GLFW", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
-   init_skia(300,300);
-    //gl_example(300,300);
-    SkCanvas* canvas = sSurface->getCanvas();
+    glfwSetFramebufferSizeCallback(window,fbCallback);
+    //glfwSetWindowSizeCallback(window,sizeCallback);
+
+    int width,height;
+    glfwGetWindowSize(window, &width, &height);
+    printf("Win initial dim %d %d\n ",width,height);
+
+    glfwGetFramebufferSize(window, &width, &height);
+    printf("FB initial dim %d %d\n ",width,height);
+
+    init_gr_context();
+    init_surface(width,height);
+    draw_content();
+    glfwSwapBuffers(window);
+
+    glfwGetWindowSize(window, &width, &height);
+    printf("Win dimensions now: %d %d\n ",width,height);
+    glfwGetFramebufferSize(window, &width, &height);
+    printf("FB dimensions now: %d %d\n ",width,height);
+    float x, y;
+    glfwGetWindowContentScale(window, &x, &y);
+    printf("scale %f %f\n",x,y);
 
     while (!glfwWindowShouldClose(window))
     {
-        int width,height;
-        glfwGetFramebufferSize(window, &width, &height);
-        canvas->clear(SK_ColorRED);
-        sContext->flush();
+        glfwWaitEvents(); // this blocks, use "poll" for non-blocking
+        draw_content();
         glfwSwapBuffers(window);
-        canvas->clear(SK_ColorRED);
-        glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
-
-//! [code]
